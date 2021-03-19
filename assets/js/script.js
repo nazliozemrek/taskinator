1971863f17222a95091188790d13f85ebb8ac351
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-03-19 14:03:39 -0500

    default browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
+
     var listItemEL =document.createElement("li");
     listItemEL.className = "task-item";
     listItemEL.textContent ="This is a new task";
     tasksToDoEl.appendChild(listItemEL);
 }
 
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
